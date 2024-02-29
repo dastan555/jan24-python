a6c9e50e869d1ec6985564d23286049f87ede4a3
dastan555 <ruslanbekov1111@gmail.com>
2024-02-29 03:13:51 +0000

    tasks

---
+++ sessions/session1.py
@@ -0,0 +1,56 @@
+
+fl = 20
+fl2 = 3.5
+print (fl // fl2)
+
+txt = '''line1 
+line2 
+line3
+line3'''
+print(txt)
+
+str1 = "hello"
+str2 = "world"
+print(str1 + str2)
+
+str = "line1\nline2\nline3"
+print (str)
+
+str = "hello world"
+print(str[10])
+
+str1 = "Strings in Python are surrounded by either single quotation marks or double quotation marks"
+print(str1[0:8])
+print(str1[11:17])
+print(str1[-5:])
+
+inp = input("what is yout name")
+print(inp)
+
+
+inp1 = input("what yur numbrer")
+inp2 = input("what yur numbrer")
+print (inp1 + inp2)
+
+inp3 = ("what ur number")
+print(inp3)
+
+inp1 = int(input("whats ur number: "))
+inp2 = int(input("whats ur number: "))
+print(inp1 + inp2)
+
+inp1 = float(input("whats ur number: "))
+inp2 = float(input("whats ur number: "))
+print(inp1 + inp2)
+
+inp1 = str(input("whats ur number: "))
+inp2 = str(input("whats ur number: "))
+print(inp1 + inp2)
+
+inp1 = bool(input("whats ur number: "))
+inp2 = bool(input("whats ur number: "))
+print(inp1 + inp2)
+
+print(len("chicago"))
+
+
