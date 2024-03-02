75d4dd59a05d98897da6a674d47bfa9e6530a822
dastan555 <ruslanbekov1111@gmail.com>
2024-03-02 18:52:47 +0000

    class task

---
+++ sessions/session3.py
@@ -0,0 +1,64 @@
+# for i in  range (10):
+#     if i == 4:
+#         break         #break --> terminates the whole loop (paired with if condition)
+#     print(i)
+
+
+# for i in range (10):
+#     if i % 2 ==1:
+#         continue          #continue --> skips the some part of loop (paired with if condition)
+#     print(i)
+# else:
+#     print("the loops is stopped")
+
+
+# for i in range(10)
+#     pass                    #pass --> the pass statement is used as a placeholder for future code
+
+
+# for i in range (4):
+#     for j in range (3):
+#         print(i)
+
+# for i in range(1,5):  
+#     for j in range(1,5):  
+#             print(i, j)
+#     print("------------")
+
+
+# for i in range (1,5):
+#     for j in range (1,5):
+#         for k in range (1,5):
+#             print(i,j,k)
+
+# jan24_names = ["aiperi", "khusrav", "fatima"] 
+#     print(jan24_names[::-1])         
+
+# jan24_names = ["aiperi", "khusrav", "fatima"] 
+# for name in jan24_names:
+#     print(name[::-1])   
+
+# numbers = ["10", "10.4", "110", "23.4"] 
+# for num in numbers:
+#     print([])
+
+# jan24_names = [
+#     "aiperi",
+#     "yrys",
+#     "abbos"
+#     ]
+# jan24_names.append("esen")
+#  print(jan24_names)
+
+jan24_names = [
+    "aiperi",
+    "yrys",
+    "abbos"
+    ]
+del jan24_names[-1]
+print(jan24_names)
+          
+
+
+    
+
