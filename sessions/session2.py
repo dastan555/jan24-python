375b354f69ee8ef734235fd4a1a7deba8c389d85
dastan555 <ruslanbekov1111@gmail.com>
2024-03-06 22:23:48 +0000

    update

--- sessions/session2.py
+++ sessions/session2.py
@@ -1,55 +1,55 @@
-# var1 = 25
-# print(bin(25))
+var1 = 25
+print(bin(25))
 
-# inp = int(input("give your number: "))
-# var = 10
-# if inp < var:
-#     print("your number is less than 10")
-# else:
-#     print("your number is greater than 10")
-
-
-# var1 = int(input("give me the number: "))
-# if var1 % 2 == 0:
-#     print("your number is even")
-# else: 
-#     print("your number is odd")
-
-# age = int(input("what is your age? "))
-
-# if age < 15:
-#     print("your a kid")
-# elif age < 20:
-#     print("your a teenager")
-# elif age < 50:
-#     print("your an adult")
-# else:
-#     print("please provide correct age")
-
-# age = int(input("what is your age? "))
-
-# if age > 0 and age < 15:
-#     print("your a kid")
-# elif age >= 15 and age < 20:
-#     print("your a teenager")
-# elif age > 20 and age < 50:
-#     print("your an adult")
-# else:
-#     print("please provide correct age")
+inp = int(input("give your number: "))
+var = 10
+if inp < var:
+    print("your number is less than 10")
+else:
+    print("your number is greater than 10")
 
 
-# inp = input("what is answer?" )
-# if inp == "n" or inp == "N":
-#     print("you answered no")
-# elif inp == "y" or inp == "Y":
-#     print("you answered yes")
+var1 = int(input("give me the number: "))
+if var1 % 2 == 0:
+    print("your number is even")
+else: 
+    print("your number is odd")
 
-# var = int(input("please enter your numbers: "))
+age = int(input("what is your age? "))
 
-# for num in range(var):
-#     if var % 2 == 0:
-#          print(num)
-#          print(var)
+if age < 15:
+    print("your a kid")
+elif age < 20:
+    print("your a teenager")
+elif age < 50:
+    print("your an adult")
+else:
+    print("please provide correct age")
+
+age = int(input("what is your age? "))
+
+if age > 0 and age < 15:
+    print("your a kid")
+elif age >= 15 and age < 20:
+    print("your a teenager")
+elif age > 20 and age < 50:
+    print("your an adult")
+else:
+    print("please provide correct age")
+
+
+inp = input("what is answer?" )
+if inp == "n" or inp == "N":
+    print("you answered no")
+elif inp == "y" or inp == "Y":
+    print("you answered yes")
+
+var = int(input("please enter your numbers: "))
+
+for num in range(var):
+    if var % 2 == 0:
+         print(num)
+         print(var)
 
 var = 0
 
--- sessions/session4.py
+++ sessions/session4.py
@@ -1,71 +1,105 @@
-# cities = ['New York', 'Los Angeles', 'Chicago', 'Huston', 'Phoenix']
-# for city in cities:
-#     print("Current city: ", city)                       # iterating list
+cities = ['New York', 'Los Angeles', 'Chicago', 'Huston', 'Phoenix']
+for city in cities:
+    print("Current city: ", city)                       # iterating list
 
 
-# turple
-# my_tuple = (1, 2, 3, 4, 5)
-# print(my_tuple[0]) 
-# print(my_tuple[-1]) 
+turple
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple[0]) 
+print(my_tuple[-1]) 
 
-# user_data = ('John', 'American', 1964, [77.0, 78.2, 77.5])
-# user_data[3].append(79.3)
-# print(user_data)
+user_data = ('John', 'American', 1964, [77.0, 78.2, 77.5])
+user_data[3].append(79.3)
+print(user_data)
 
-# Answer: ('John', 'American', 1964, [77.0, 78.2, 77.5, 79.3])
-
-
-         #dictionary
-# my_dict = {'name': 'John', 'age': 25, 'city': 'New York'}               
-# print(my_dict)
-
-# my_dict = {'name': 'John', 'age': 25, 'city': 'New York'}
-# print(my_dict['name'])
+Answer: ('John', 'American', 1964, [77.0, 78.2, 77.5, 79.3])
 
 
-# my_dict = {'name': 'John', 'age': 25, 'city': 'New York'}
-# my_dict['age'] = 26
-# print(my_dict)
+         dictionary
+my_dict = {'name': 'John', 'age': 25, 'city': 'New York'}               
+print(my_dict)
 
-# movie1 = [
-#     {
-#         "name": "Manas",
-#         "year": "2022",
-#         "direcotor": "ruslan akun",
-#         "actor": ["emil", "samat"],
-#     }
-# ]    
-# print(movie1)
+my_dict = {'name': 'John', 'age': 25, 'city': 'New York'}
+print(my_dict['name'])
 
-# movie1 ={
+
+my_dict = {'name': 'John', 'age': 25, 'city': 'New York'}
+my_dict['age'] = 26
+print(my_dict)
+
+movie1 = [
+    {
+        "name": "Manas",
+        "year": "2022",
+        "direcotor": "ruslan akun",
+        "actor": ["emil", "samat"],
+    }
+]    
+print(movie1)
+
+movie1 ={
     
-#     "name": "Manas",
-#     "year": "2022",
-#     "direcotor": "ruslan akun",
-#     "actor": ["emil", "samat"],
-# }
-#     #.values    #.keys
-# for info in movie1.items():      
-#     print(info)         
+    "name": "Manas",
+    "year": "2022",
+    "direcotor": "ruslan akun",
+    "actor": ["emil", "samat"],
+}
+    #.values    #.keys
+for info in movie1.items():      
+    print(info)         
 
           #set 
-# st = {1,1,1,2,2,2,3,3,3,4,"hello", "hello"}
-# print(st)
+st = {1,1,1,2,2,2,3,3,3,4,"hello", "hello"}
+print(st)
 
-# my_set = {2, 1, 3, 4, 5,}   #sets - always remove duplicates and sort them
-# print(my_set)
+my_set = {2, 1, 3, 4, 5,}   #sets - always remove duplicates and sort them
+print(my_set)
 
 
-# set_a = {1,2,3,4}
-# set_b = {4,5,6,7}
-# union_set = set_a | set_b    # union_set "&" "|"
-# print(union_set)
+set_a = {1,2,3,4}
+set_b = {4,5,6,7}
+union_set = set_a | set_b    # union_set "&" "|"
+print(union_set)
 
-#comprehansion
-# lst = []
-# for num in range(10):
-#     lst.append(num)
-# print(lst)
+comprehansion
+lst = []
+for num in range(10):
+    lst.append(num)
+print(lst)
 
 lst = [char for char in "hello" ]   ### [value | for loop]
 print(lst)
+
+
+        #lists --- the list is changable, meaning that we can change,add,remove after it has been created
+
+# cities = ['new york', 'los angeles', 'boston', 'dallas', 'chicago']
+# print(cities)
+# print(cities[0])            #lists are created using square brackets
+# print(cities[-1])
+# print(cities[3:])
+
+# cities = ['new york', 'los angeles', 'boston', 'dallas', 'chicago']
+# del cities [0]              ##del ---> deleting element in the list 
+# print(cities)               
+
+# book_ratings = [1,4,5,6,7,8]
+# book_ratings.append(2)           #append ---> adds an element to the end of a list
+# print(book_ratings)
+
+# book_ratings = [2,4,6,8]
+# book_ratings.insert(4,10)             #insert ---> insert an element at a specific index in a list
+# print(book_ratings)
+
+
+# countries = ['usa', 'china', 'brazil', 'cuba']
+# for country in countries:                              # ---> iteratinh
+#     print("curent country: ", country)
+
+
+# cities = ['New York', 'Los Angeles', 'Chicago', 'Huston', 'Phoenix']
+# for index in range(len(cities)):
+#     print("current index:", index, '| Current city: ', cities[index])
+
+# thislist = ["apple", "banana", "cherry"]
+# print(len(thislist[0]))
